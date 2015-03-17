05816e4e35840508c6a534be80024b99e22ec214
Hayley Swimelar <hswimelar@gmail.com>
2015-03-17 16:51:42 -0700

    remove unnessesary substringify function

--- substrings.rb
+++ substrings.rb
@@ -1,21 +1,3 @@
-# Takes a string an converts it into an array of its substrings
-def substringify(string)
-  # Removes all non word characters from the string
-  string.tr!(string.scan(/\W/).to_s, ' ')
-
-  all_indexes_of_string = (0..string.length - 1).to_a
-  substrings = []
-
-  # All possible combinations of string's indexes
-  substring_indexs = all_indexes_of_string.product(all_indexes_of_string)
-
-  # Push all possible left to right substrings onto substrings array.
-  substring_indexs.each do |a|
-    substrings << string.slice(a[0]..a[1]) if a[0] <= a[1]
-  end
-  substrings
-end
-
 # Takes a string and a dictionary of possible substrings and returns
 # elements of the dictionary that are substrings of the string
 # and how many times they are present in the dictionary.
